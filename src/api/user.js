692881fe43f1b5034c3201b1585fa3e1a386567a
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2023-04-17 20:06:48 +0300

    add test
    add test

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'https://followers-backend.herokuapp.com/',
-  // baseURL: 'http://localhost:4000',
+  // baseURL: 'https://followers-backend.herokuapp.com/',
+  baseURL: 'http://localhost:4000',
 });
 
 export const axiosAddUser = async userData => {
